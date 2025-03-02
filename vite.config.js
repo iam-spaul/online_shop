34b402f37c0eca6b06cde83d426dbd0d4b3e7675
Subhankar Paul <psubhankar2019@gmail.com>
2025-03-02 23:51:48 +0530

    fix: update Vite configuration for base path and retain React plugin

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: './',
-    css: {
-      devSourcemap: false
-    }
-})
+  base: "/onlineshop/",
+  plugins: [react()],
+});
